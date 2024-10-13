80635b08b3720e20e41196419e63574f36c2fa94
Gabe <gabepineda6@gmail.com>
2024-10-12 18:42:40 -0700

    Added a delay before display the exxit message and exiting the program

--- src/main/java/com/pluralsight/Quit.java
+++ src/main/java/com/pluralsight/Quit.java
@@ -1,8 +1,10 @@
 package com.pluralsight;
 
 public class Quit {
-    public static void Exit(){
-        System.out.println("Thank you for using LedgeMaxxing! Please use our app again!");
+    public static void Exit() throws InterruptedException {
+        Thread.sleep(500);
+        System.out.println("\nThank you for using LedgeMaxxing! Please use our app again!");
+        Thread.sleep(500);
         System.exit(0);
     }
 }
