2c5a58855b172052c8b5470a56cc27c54c5ed447
Gabe <gabepineda6@gmail.com>
2024-10-12 19:50:15 -0700

    Removed the switch case as it was not necesarry to find out if the user wanted a custom date or not . Prompted the user for the info that will go inside of the ledger

--- src/main/java/com/pluralsight/Deposit.java
+++ src/main/java/com/pluralsight/Deposit.java
@@ -16,25 +16,20 @@ public class Deposit {
         System.out.println("\nPlease have the deposit description, vendor, and amount at the ready." +
                 "\nIf using a custom date and time have please have that ready as well." );
 
+        String description = ans("\nPlease enter a description for your deposit: ");
+
+        String vendor = ans("\nPlease enter the vendor for your deposit");
+
+        String amount = ans("Please enter the amount for your deposit");
+
         String dt = ans("\nWould you like to use a custom date and time for this deposit? Enter yes or no: " ).toUpperCase();
 
-        switch (dt) {
-            case "YES":
-                System.out.println("Custom");
-                break;
-            case "NO":
-                System.out.println("System");
-                break;
-            case "H":
-                HomeScreen();
-                break;
-            case "X":
-                Exit();
-                break;
-            default:
-                System.out.println("Sorry! That was not a proper choice. Try again!");
-
+        if(dt.equals("YES")){
+            String d = ans("\nPlease enter the custom date for your deposit in MM-DD-YYYY format: ");
+            String t = ans("\nPlease enter the custom time for your deposit in HH:MM format: ");
         }
 
     }
+
+
 }
