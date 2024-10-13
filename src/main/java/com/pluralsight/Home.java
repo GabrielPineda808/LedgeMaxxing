ebd30a6740ccd5f16af2a1e3be1f0a2eb09e82b4
Gabe <gabepineda6@gmail.com>
2024-10-12 18:18:54 -0700

    Moved all screen function to their own java class to keep the main app clean.

---
+++ src/main/java/com/pluralsight/Home.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+import static com.pluralsight.Deposit.AddDeposit;
+
+public class Home {
+    static Scanner s = new Scanner(System.in);
+    public static void homeScreen(){
+        System.out.print("Welcome to LedgeMaxxer! Thank you for using our service. \n" +
+                "Please enter a character that corresponds with one of the options below.\n" +
+                "D) Add Deposit\n" +
+                "P) Make Payment(Debit)\n" +
+                "L) Ledger\n" +
+                "X) Exit\n" +
+                "Enter here: ");
+
+        String ans = s.nextLine();
+        switch (ans.toUpperCase()){
+            case "A":
+                AddDeposit();
+                break;
+            default:
+                System.out.println("Sorry! We do not have that option. Please input another character.");
+                homeScreen();
+        }
+    }
+
+
+}
--- src/main/java/com/pluralsight/LedgeMaxxing.java
+++ src/main/java/com/pluralsight/LedgeMaxxing.java
@@ -1,32 +1,10 @@
 package com.pluralsight;
 import java.util.Scanner;
+import static com.pluralsight.Home.HomeScreen;
+
 public class LedgeMaxxing {
     static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         HomeScreen();
-    }
-
-    public static void HomeScreen(){
-        System.out.print("Welcome to LedgeMaxxer! Thank you for using our service. \n" +
-                "Please enter a character that corresponds with one of the options below.\n" +
-                "D) Add Deposit\n" +
-                "P) Make Payment(Debit)\n" +
-                "L) Ledger\n" +
-                "X) Exit\n" +
-                "Enter here: ");
-
-        String ans = s.nextLine();
-        switch (ans.toUpperCase()){
-            case "A":
-                AddDeposit();
-                break;
-            default:
-                System.out.println("Sorry! We do not have that option. Please input another character.");
-                HomeScreen();
-        }
-    }
-
-    public static void AddDeposit(){
-        System.out.println("Add Deposit Screen");
     }
 }
