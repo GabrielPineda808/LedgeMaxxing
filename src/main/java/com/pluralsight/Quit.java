aaad9339f3569f7c110594c75ec6b1a42feba347
Gabe <gabepineda6@gmail.com>
2024-10-12 18:31:30 -0700

    Created a quit class that contains an exit method that displays a thank you message before quitting the program

--- src/main/java/com/pluralsight/Quit.java
+++ src/main/java/com/pluralsight/Quit.java
@@ -1,4 +1,8 @@
 package com.pluralsight;
 
 public class Quit {
+    public static void Exit(){
+        System.out.println("Thank you for using LedgeMaxxing! Please use our app again!");
+        System.exit(0);
+    }
 }
