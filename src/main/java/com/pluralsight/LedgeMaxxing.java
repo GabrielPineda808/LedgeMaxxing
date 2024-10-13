d8c3d5834a523e30f2cf57cc57529e2c21f350e7
Gabe <gabepineda6@gmail.com>
2024-10-12 18:09:55 -0700

    Created an Add Deposit method/screen that the user has access to

--- src/main/java/com/pluralsight/LedgeMaxxing.java
+++ src/main/java/com/pluralsight/LedgeMaxxing.java
@@ -7,14 +7,26 @@ public class LedgeMaxxing {
     }
 
     public static void HomeScreen(){
-        System.out.println("Welcome to LedgeMaxxer! Thank you for using our service. \n" +
+        System.out.print("Welcome to LedgeMaxxer! Thank you for using our service. \n" +
                 "Please enter a character that corresponds with one of the options below.\n" +
                 "D) Add Deposit\n" +
                 "P) Make Payment(Debit)\n" +
                 "L) Ledger\n" +
                 "X) Exit\n" +
-                "Thank you!");
+                "Enter here: ");
 
         String ans = s.nextLine();
+        switch (ans.toUpperCase()){
+            case "A":
+                AddDeposit();
+                break;
+            default:
+                System.out.println("Sorry! We do not have that option. Please input another character.");
+                HomeScreen();
+        }
+    }
+
+    public static void AddDeposit(){
+        System.out.println("Add Deposit Screen");
     }
 }
