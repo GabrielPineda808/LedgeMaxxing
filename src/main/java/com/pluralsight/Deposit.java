bbb5b0e564d2bf477d3300835235cab030bd1769
Gabe <gabepineda6@gmail.com>
2024-10-12 19:01:45 -0700

    Let the user know they are in the deposit screen. Asked user if they wanted to use a custom time or date

--- src/main/java/com/pluralsight/Deposit.java
+++ src/main/java/com/pluralsight/Deposit.java
@@ -1,7 +1,21 @@
 package com.pluralsight;
 
+import static com.pluralsight.Home.ans;
+
 public class Deposit {
-    public static void AddDeposit(){
-        System.out.println("Add Deposit Screen");
+    public static void AddDeposit() throws InterruptedException {
+        Thread.sleep(500);
+
+        System.out.println("\nYou are now on the Add Deposit Screen!");
+
+        System.out.println("\nPlease have the deposit description, vendor, and amount at the ready." +
+                "\nIf using a custom date and time have please have that ready as well." );
+
+        String dt = ans("\nWould you like to use a custom date and time for this deposit? Enter yes or no: " ).toUpperCase();
+        if(dt.equals("YES")){
+            System.out.println("\nCustom date");
+        }else {
+            System.out.println("\nSystem date");
+        }
     }
 }
