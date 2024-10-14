80b18b979d0834837f32804ba4e448c859c892b9
Gabe <gabepineda6@gmail.com>
2024-10-14 15:47:47 -0700

    Prompted the user a menu of options and saved the user input

--- src/main/java/com/pluralsight/LedgerScreen.java
+++ src/main/java/com/pluralsight/LedgerScreen.java
@@ -1,7 +1,22 @@
 package com.pluralsight;
 
+import static com.pluralsight.Home.ans;
+
 public class LedgerScreen {
+    public static void main(String[] args) {
+        Ledger();
+    }
     public static void Ledger(){
-        System.out.println("Ledger Screen.");
+        System.out.println("\nYou are on the Make Payment Screen!" +
+                "\nEnter h (Return to home) or x (Exit app) at any time!");
+
+        System.out.println("Please enter one of the following options: " +
+                "\nA ) All - display all entries" +
+                "\nD ) Deposits - display all deposits only" +
+                "\nP ) Payments - display all payments only" +
+                "\nR ) Reports - view default reports or custom reports");
+        String choice = ans("\nEnter here:");
+
+
     }
 }
