65432c186f60e0d723318d80ac3d9ff5e39b2d0d
Gabe <gabepineda6@gmail.com>
2024-10-14 17:48:32 -0700

    Created a file reader to take in the transaction csv in order to display the report. Currently added the month to date report that will grab the current year and month the user is in that will check the csv for transactionsthat contain the current month and year

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -1,14 +1,21 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 import static com.pluralsight.Deposit.inputChecker;
 import static com.pluralsight.Home.ans;
 import static com.pluralsight.LedgerScreen.Ledger;
 
 public class Reports {
+    static LocalDateTime time = LocalDateTime.now();
+    static String year = Integer.toString(time.getYear());
+    static String month = Integer.toString(time.getMonthValue());
+
     public static void reports() throws IOException, InterruptedException {
-        System.out.println("\nYou are on the Ledger Screen!" +
+        System.out.println("\nYou are on the Reports Screen!" +
                 "\nEnter h (Return to home) or x (Exit app) at any time!");
 
         System.out.println("Please enter one of the following options: " +
@@ -22,6 +29,7 @@ public class Reports {
         inputChecker(choice);
         switch (choice){
             case "1":
+                monthToDate();
                 break;
             case "2":
                 break;
@@ -36,6 +44,20 @@ public class Reports {
                 break;
 
 
+        }
+    }
+
+    public static void monthToDate() throws IOException {
+        FileReader fr= new FileReader("transactions.csv");
+        BufferedReader br = new BufferedReader(fr);
+        String input;
+        while((input = br.readLine())!= null){
+            String[] user = input.split("\\|");
+            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
+            String[] userTime= x.getDate().split("-");
+            if(userTime[0].contains(year) && userTime[1].contains(month)){
+                System.out.println(x.display());
+            }
         }
     }
 }
