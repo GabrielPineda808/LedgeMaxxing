701e7f3cdb80be30ce934e92226f9cb49b0136cb
Gabe <gabepineda6@gmail.com>
2024-10-14 17:11:41 -0700

    Checked to see if the transactions csv is there if not it creates one with the proper table headers

--- src/main/java/com/pluralsight/LedgeMaxxing.java
+++ src/main/java/com/pluralsight/LedgeMaxxing.java
@@ -1,9 +1,20 @@
 package com.pluralsight;
-import java.io.IOException;
+import java.io.*;
+
 import static com.pluralsight.Home.HomeScreen;
 
 public class LedgeMaxxing {
     public static void main(String[] args) throws InterruptedException, IOException {
-        HomeScreen();
+
+        if(new File("transactions.csv").isFile()){
+            HomeScreen();
+        }else {
+            FileWriter s = new FileWriter("transactions.csv",true);
+            BufferedWriter br = new BufferedWriter(s);
+
+            br.write("DATE | TIME | DESCRIPTION | VENDOR | AMOUNT ");
+            br.close();
+            HomeScreen();
+        }
     }
 }
