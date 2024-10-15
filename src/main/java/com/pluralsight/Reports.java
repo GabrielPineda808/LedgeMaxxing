6ba926f001a208bfe3dc19333a1828743f2afb3a
Gabe <gabepineda6@gmail.com>
2024-10-14 18:06:15 -0700

    Created methods for the user to search between mtd, revious month, ytd, previousyear, and a vendor search. need to reformat the code as there is duplicate code

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -13,6 +13,8 @@ public class Reports {
     static LocalDateTime time = LocalDateTime.now();
     static String year = Integer.toString(time.getYear());
     static String month = Integer.toString(time.getMonthValue());
+    static String pYear = Integer.toString(time.minusYears(1).getYear());
+    static String pMonth = Integer.toString(time.minusMonths(1).getMonthValue());
 
     public static void reports() throws IOException, InterruptedException {
         System.out.println("\nYou are on the Reports Screen!" +
@@ -32,12 +34,16 @@ public class Reports {
                 monthToDate();
                 break;
             case "2":
+                previousMonth();
                 break;
             case "3":
+                ytd();
                 break;
             case "4":
+                previousYear();
                 break;
             case "5":
+                byVendor();
                 break;
             case "0":
                 Ledger();
@@ -56,6 +62,62 @@ public class Reports {
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
             String[] userTime= x.getDate().split("-");
             if(userTime[0].contains(year) && userTime[1].contains(month)){
+                System.out.println(x.display());
+            }
+        }
+    }
+
+    public static void previousMonth() throws IOException {
+        FileReader fr= new FileReader("transactions.csv");
+        BufferedReader br = new BufferedReader(fr);
+        String input;
+        while((input = br.readLine())!= null){
+            String[] user = input.split("\\|");
+            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
+            String[] userTime= x.getDate().split("-");
+            if(userTime[0].contains(year) && userTime[1].contains(pMonth)){
+                System.out.println(x.display());
+            }
+        }
+    }
+    public static void ytd() throws IOException {
+        FileReader fr= new FileReader("transactions.csv");
+        BufferedReader br = new BufferedReader(fr);
+        String input;
+        while((input = br.readLine())!= null){
+            String[] user = input.split("\\|");
+            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
+            String[] userTime= x.getDate().split("-");
+            if(userTime[0].contains(year)){
+                System.out.println(x.display());
+            }
+        }
+    }
+
+    public static void previousYear() throws IOException {
+        FileReader fr= new FileReader("transactions.csv");
+        BufferedReader br = new BufferedReader(fr);
+        String input;
+        while((input = br.readLine())!= null){
+            String[] user = input.split("\\|");
+            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
+            String[] userTime= x.getDate().split("-");
+            if(userTime[0].contains(pYear)){
+                System.out.println(x.display());
+            }
+        }
+    }
+    public static void byVendor() throws IOException {
+
+        String v = ans("Please enter the VENDOR: ");
+        FileReader fr= new FileReader("transactions.csv");
+        BufferedReader br = new BufferedReader(fr);
+        String input;
+        while((input = br.readLine())!= null){
+            String[] user = input.split("\\|");
+            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
+
+            if(x.getVendor().toUpperCase().contains(v.toUpperCase())){
                 System.out.println(x.display());
             }
         }
