669ad6b25d209331dc66bf4f772ff8fda28e47ce
Gabe <gabepineda6@gmail.com>
2024-10-14 17:17:13 -0700

    Prompted the user to select one of the follow predefined reports and added a switch case for each

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
+import java.io.IOException;
+
+import static com.pluralsight.Deposit.inputChecker;
+import static com.pluralsight.Home.ans;
+import static com.pluralsight.LedgerScreen.Ledger;
+
 public class Reports {
+    public static void reports() throws IOException, InterruptedException {
+        System.out.println("\nYou are on the Ledger Screen!" +
+                "\nEnter h (Return to home) or x (Exit app) at any time!");
+
+        System.out.println("Please enter one of the following options: " +
+                "\n1 ) Month to Date" +
+                "\n2 ) Previous Month" +
+                "\n3 ) Year to Date" +
+                "\n4 ) Previous Year" +
+                "\n5 ) Search by Vendor" +
+                "\n0 ) Return to Ledger Screen");
+        String choice = ans("\nEnter here:").toUpperCase();
+        inputChecker(choice);
+        switch (choice){
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            case "5":
+                break;
+            case "0":
+                Ledger();
+                break;
+
+
+        }
+    }
 }
