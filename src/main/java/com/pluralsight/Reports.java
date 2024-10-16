dd04b037a8219a86835ec5dcb69222bd4080ea38
Gabe <gabepineda6@gmail.com>
2024-10-15 17:58:01 -0700

    Created a method that will read the file so that code is not duplicated

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -9,6 +9,7 @@ import static com.pluralsight.Deposit.inputChecker;
 import static com.pluralsight.Home.HomeScreen;
 import static com.pluralsight.Home.ans;
 import static com.pluralsight.LedgerScreen.Ledger;
+import static com.pluralsight.LedgerScreen.fr;
 
 public class Reports {
     static LocalDateTime time = LocalDateTime.now();
@@ -16,6 +17,7 @@ public class Reports {
     static String month = Integer.toString(time.getMonthValue());
     static String pYear = Integer.toString(time.minusYears(1).getYear());
     static String pMonth = Integer.toString(time.minusMonths(1).getMonthValue());
+    static String input;
 
     public static void reports() throws IOException, InterruptedException {
         System.out.println("\nYou are on the Reports Screen!" +
@@ -49,15 +51,17 @@ public class Reports {
             case "0":
                 Ledger();
                 break;
-
+            default:
+                System.out.println("That is a not a valid report option. Please try again.");
+                reports();
 
         }
     }
 
     public static void monthToDate() throws IOException, InterruptedException {
-        FileReader fr= new FileReader("transactions.csv");
-        BufferedReader br = new BufferedReader(fr);
-        String input;
+
+        BufferedReader br = new BufferedReader(fr());
+
         while((input = br.readLine())!= null){
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
@@ -70,9 +74,9 @@ public class Reports {
     }
 
     public static void previousMonth() throws IOException, InterruptedException {
-        FileReader fr= new FileReader("transactions.csv");
-        BufferedReader br = new BufferedReader(fr);
-        String input;
+
+        BufferedReader br = new BufferedReader(fr());
+
         while((input = br.readLine())!= null){
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
@@ -84,9 +88,9 @@ public class Reports {
         option();
     }
     public static void ytd() throws IOException, InterruptedException {
-        FileReader fr= new FileReader("transactions.csv");
-        BufferedReader br = new BufferedReader(fr);
-        String input;
+
+        BufferedReader br = new BufferedReader(fr());
+
         while((input = br.readLine())!= null){
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
@@ -99,9 +103,9 @@ public class Reports {
     }
 
     public static void previousYear() throws IOException, InterruptedException {
-        FileReader fr= new FileReader("transactions.csv");
-        BufferedReader br = new BufferedReader(fr);
-        String input;
+
+        BufferedReader br = new BufferedReader(fr());
+
         while((input = br.readLine())!= null){
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
@@ -115,9 +119,9 @@ public class Reports {
     public static void byVendor() throws IOException, InterruptedException {
 
         String v = ans("\nPlease enter the VENDOR: ");
-        FileReader fr= new FileReader("transactions.csv");
-        BufferedReader br = new BufferedReader(fr);
-        String input;
+
+        BufferedReader br = new BufferedReader(fr());
+        
         while((input = br.readLine())!= null){
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
@@ -132,6 +136,8 @@ public class Reports {
     public static void option() throws IOException, InterruptedException {
 
         String another = ans("\nWould you like to view another report? Enter yes or no: ");
+        inputChecker(another);
+
         switch (another.toUpperCase()){
             case "YES":
                 reports();
