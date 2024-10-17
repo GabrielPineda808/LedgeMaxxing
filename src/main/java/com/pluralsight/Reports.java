8ef1f16c0c288bec16d0c6ad93cf50a2e2b85c54
Gabe <gabepineda6@gmail.com>
2024-10-17 12:53:34 -0700

    Created an if statement that checks if the vendor is found in any transactions if not it is known

--- src/main/java/com/pluralsight/Reports.java
+++ src/main/java/com/pluralsight/Reports.java
@@ -1,22 +1,19 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.time.LocalDateTime;
-
 import static com.pluralsight.Deposit.inputChecker;
-import static com.pluralsight.Home.HomeScreen;
 import static com.pluralsight.Home.ans;
 import static com.pluralsight.LedgerScreen.Ledger;
 import static com.pluralsight.LedgerScreen.fr;
 
 public class Reports {
-    static LocalDateTime time = LocalDateTime.now();
-    static String year = Integer.toString(time.getYear());
-    static String month = Integer.toString(time.getMonthValue());
-    static String pYear = Integer.toString(time.minusYears(1).getYear());
-    static String pMonth = Integer.toString(time.minusMonths(1).getMonthValue());
+    static LocalDateTime time = LocalDateTime.now(); //grabbing the current date
+    static String year = Integer.toString(time.getYear()); // grabbing the current year
+    static String month = Integer.toString(time.getMonthValue());//getting the current month
+    static String pYear = Integer.toString(time.minusYears(1).getYear());//getting whatever the previous year is
+    static String pMonth = Integer.toString(time.minusMonths(1).getMonthValue());//getting the previous month
     static String input;
 
     public static void reports() throws IOException, InterruptedException {
@@ -30,9 +27,9 @@ public class Reports {
                 "\n4 ) Previous Year" +
                 "\n5 ) Search by Vendor" +
                 "\n0 ) Return to Ledger Screen");
-        String choice = ans("\nEnter here: ").toUpperCase();
+        String choice = ans("\nEnter here: ").toUpperCase(); //grabbing user input
         inputChecker(choice);
-        switch (choice){
+        switch (choice){ // switch case to call on the method tht will perform what the user wants
             case "1":
                 monthToDate();
                 break;
@@ -53,24 +50,24 @@ public class Reports {
                 break;
             default:
                 System.out.println("\nThat is a not a valid report option. Please try again.\n");
-                reports();
+                reports(); //re runs the report page if the input is not valid
 
         }
     }
 
-    public static void monthToDate() throws IOException, InterruptedException {
+    public static void monthToDate() throws IOException, InterruptedException { //method to grab every transaction in the current month
 
-        BufferedReader br = new BufferedReader(fr());
+        BufferedReader br = new BufferedReader(fr());//loading the transaction file
 
-        while((input = br.readLine())!= null){
-            String[] user = input.split("\\|");
-            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
-            String[] userTime= x.getDate().split("-");
-            if(userTime[0].contains(year) && userTime[1].contains(month)){
-                System.out.println("\n"+ x.display()+ "\n");
+        while((input = br.readLine())!= null){ //while loop to go through each line in transaction csv
+            String[] user = input.split("\\|");//creating an array splitting the transaction by table header
+            Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);//creating an object with our array
+            String[] userTime= x.getDate().split("-");// grabbing the date and splitting it into an array
+            if(userTime[0].contains(year) && userTime[1].contains(month)){//if the transaction date contains the current uear and month
+                System.out.println("\n"+ x.display()+ "\n");//it will print
             }
         }
-        option();
+        option();// asking the user if they eant to run another option in the report page
     }
 
     public static void previousMonth() throws IOException, InterruptedException {
@@ -81,8 +78,8 @@ public class Reports {
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
             String[] userTime= x.getDate().split("-");
-            if(userTime[0].contains(year) && userTime[1].contains(pMonth)){
-                System.out.println("\n"+ x.display()+ "\n");
+            if(userTime[0].contains(year) && userTime[1].contains(pMonth)){//if the user contains this year and whatever the previous month may be
+                System.out.println("\n"+ x.display()+ "\n");//displays if there
             }
         }
         option();
@@ -95,8 +92,8 @@ public class Reports {
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
             String[] userTime= x.getDate().split("-");
-            if(userTime[0].contains(year)){
-                System.out.println("\n"+ x.display()+ "\n");
+            if(userTime[0].contains(year)){//if the transaction contains the current year
+                System.out.println("\n"+ x.display()+ "\n");//displays if available
             }
         }
         option();
@@ -110,30 +107,34 @@ public class Reports {
             String[] user = input.split("\\|");
             Transaction x = new Transaction(user[0],user[1],user[2],user[3],user[4]);
             String[] userTime= x.getDate().split("-");
-            if(userTime[0].contains(pYear)){
+            if(userTime[0].contains(pYear)){//grabbing all transactions that contain the previous year in the date
                 System.out.println("\n"+ x.display()+ "\n");
             }
         }
         option();
     }
     public static void byVendor() throws IOException, InterruptedException {
-
+        boolean vendorNotFound = true;
         String v = ans("\nPlease enter the VENDOR: ");
-
         BufferedReader br = new BufferedReader(fr());
-
             while ((input = br.readLine()) != null) {
+
                 String[] user = input.split("\\|");
                 Transaction x = new Transaction(user[0], user[1], user[2], user[3], user[4]);
 
-                if (x.getVendor().toUpperCase().contains(v.toUpperCase())) {
-                    System.out.println("\n" + x.display() + "\n");
+                if (x.getVendor().toUpperCase().contains(v.toUpperCase())) { // if the transaction contains the user inputted vendor
+                    System.out.println("\n" + x.display());
+                    vendorNotFound = false;
                 }
             }
+            if (vendorNotFound){
+                System.out.println("\nThere is no vendor that contains " + v + "\n");
+            }
+
         option();
     }
 
-    public static void option() throws IOException, InterruptedException {
+    public static void option() throws IOException, InterruptedException { // method that asks the user if they want to re run the optons page
 
         String another = ans("\nWould you like to view another report? Enter yes or no: ");
         inputChecker(another);
