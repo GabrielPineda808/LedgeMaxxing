94be29a34a5358f38a85f18cc8c08c575416be35
Gabe <gabepineda6@gmail.com>
2024-10-17 09:38:10 -0700

    reformatting code and adding comments

--- src/main/java/com/pluralsight/Deposit.java
+++ src/main/java/com/pluralsight/Deposit.java
@@ -9,14 +9,14 @@ public class Deposit {
 
     public static void AddDeposit() throws InterruptedException, IOException {
 
-        userMessage("deposit","");
+        userMessage("deposit",""); // calls on userMessage method
     }
 
-    public static void inputChecker(String input) throws IOException, InterruptedException {
-        if(input.equalsIgnoreCase("H")){
-            HomeScreen();
-        }else if(input.equalsIgnoreCase("x")){
-            Exit();
+    public static void inputChecker(String input) throws IOException, InterruptedException { //checks user input to see if they want to go back to the homescreen or exit the app at any time
+        if(input.equals("HOME")){
+            HomeScreen(); // calls home screen method
+        }else if(input.equals("EXIT")){
+            Exit(); //calls exit method
         }
 
     }
